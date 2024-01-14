6942014d746c43929dda7e4ba95ca97e8dc652ab
Luka <lp2499@student.uni-lj.si>
2024-01-14 11:20:58 +0100

    osvetljava + ozadje + spawnerpopravki

--- Assets/Scripts/DustSpawner.cs
+++ Assets/Scripts/DustSpawner.cs
@@ -11,8 +11,11 @@ public class DustSpawner : MonoBehaviour
         for(int i = 0; i < DustCount; i++){
         int spawnPointX = Random.Range(-20, 20);
         int spawnPointY = 0;
-        int spawnPointZ = Random.Range(-20, 20);
-
+        int spawnPointZ = Random.Range(-28, 20);
+            if (spawnPointZ >= -19 && spawnPointZ < -15){
+    // Move to the next range (15, 20)
+    spawnPointZ = Random.Range(-14, 20);
+        }       
         Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, spawnPointZ);
 
         Instantiate(dustPrefab, spawnPosition, Quaternion.identity);
